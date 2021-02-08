02b3b31eaa21b95d0af312f9dd8de95d38e9a485
Lala5th <lala5th@gmail.com>
2021-02-08 19:36:02 +0000

    Added normaised plot file

---
+++ runs/plotEDepNorm.py
@@ -0,0 +1,25 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+
+args = sys.argv
+cmap = cm.get_cmap('gnuplot')
+
+dir = args[1]
+
+E = np.load(dir + '/InitialEnergy.npy')['0']
+y = np.load(dir + '/y.npy')['0']
+z = np.load(dir + '/z.npy')['0']
+data = np.reshape(np.load(dir + '/DepositedEnergy.npy'),(len(E),len(y),len(z)))
+
+norm = colors.Normalize(vmin=0,vmax=max(E))
+
+plt.ion()
+plt.figure()
+[plt.plot(z,np.sum(data['1'][i,:,:],axis = 0)/max(np.sum(data['1'][i,:,:],axis = 0)),color=cmap(norm(E[i]))) for i in range(len(E))]
+plt.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap)).set_label('Initial Energy [MeV]')
+plt.ylabel('Normalised deposited Energy [a.u.]')
+plt.xlabel('Longitudinal crystal ID')
+plt.show()
