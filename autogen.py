d8a23f1f448a5adf57c139fe2361ba870cc09473
Lala5th <lala5th@gmail.com>
2021-03-01 19:34:42 +0000

    Updated autogen

--- autogen.py
+++ autogen.py
@@ -6,7 +6,23 @@ file = open("autogen.mac","w+")
 
 file.write(init);
 
-for E in range(10,1010,10):
+E = 0.1
+Es = []
+while(E < 1000):
+    Es.append(E)
+    if E < 10:
+        E += 0.1
+        continue
+    if E < 50:
+        E += 1
+        continue
+    if E < 500:
+        E += 10
+        continue
+    E += 25
+
+
+for E in Es:
     file.write(f"/gun/energy {E} MeV\n")
     file.write("/run/beamOn 8\n")
 
