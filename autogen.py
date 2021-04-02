c2673fe647574e1c60bc641b084108c22be18fc4
Lala5th <lala5th@gmail.com>
2021-04-02 10:41:17 +0100

    Added test for Einit vs Convolution

--- autogen.py
+++ autogen.py
@@ -3,9 +3,10 @@ import numpy as np
 
 file = open("autogen.mac","w+")
 
-for EC in np.arange(-0.4,0.4,0.1):
-    file.write(f"/mangling/setEC {EC}\n")
-    for std in range(0,50,5):
+file.write(f"/mangling/setEC 0\n")
+for E in range(10,300,10):
+    file.write(f"/gun/energy {E} MeV\n")
+    for std in range(0,200,10):
         file.write(f"/mangling/setStd {std}\n")
         file.write("/run/beamOn 8\n")
 
