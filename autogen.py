03c1b4854923058dca1dc808bf0c9c44c304d19a
Lala5th <lala5th@gmail.com>
2021-04-04 01:19:17 +0100

    Reset wider test range

--- autogen.py
+++ autogen.py
@@ -4,9 +4,9 @@ import numpy as np
 file = open("autogen.mac","w+")
 
 file.write(f"/mangling/setEC 0\n")
-for E in range(10,200,20):
+for E in range(10,300,10):
     file.write(f"/gun/energy {E} MeV\n")
-    for std in range(0,200,40):
+    for std in range(0,200,10):
         file.write(f"/mangling/setStd {std}\n")
         file.write("/run/beamOn 8\n")
 
